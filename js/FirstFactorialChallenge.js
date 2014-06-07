4849039c9835eda75e346321375f162eaefd2cfb
Gitthere <ichi4g@gmail.com>
2014-06-07 10:23:05 -1000

    completed input check to see if number and positive number

--- js/FirstFactorialChallenge.js
+++ js/FirstFactorialChallenge.js
@@ -1,3 +1,24 @@
 
+//CHECK IF INPUT IS NUMBER
 
+var button = document.getElementById('getAnswer');
+
+button.onclick = function checkInput () {
+  var integerCheck = document.getElementById('userInput').value;
+  if (isNaN(userInput) || userInput <= 0) {
+    alert("Must input positive numbers.");
+    return false;
+  }
+}
+
+
+//SET UP FUNCTION TO CALCULATE FACTORIAL
+// function() {
+//   var factorial = document.getElementById('userInput')
+//   for (factorial=factorial; factorial > 0; factorial--);
+//   factorial = userInput * userInput-1 * ...
+// }
+
+
+//RETURN FACTORIAL
 
